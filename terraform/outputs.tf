a369b1bf16dcca87563dcd91563dd97671a483d6
Karandeep Singh <karandeep.singh@tothenew.com>
2025-08-24 15:26:02 +0530

    initial commit

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
-  value       = length(aws_instance.web) > 0 ? aws_instance.web[0].id : ""
-  description = "ID of the EC2 instance (empty if not created)"
+  value       = length(aws_instance.web) > 0 ? aws_instance.web[0].id : null
+  description = "ID of the EC2 instance (null if not created)"
 }
 
 output "public_ip" {
-  value       = length(aws_instance.web) > 0 ? aws_instance.web[0].public_ip : ""
-  description = "Public IP of the EC2 instance (empty if not created)"
+  value       = length(aws_instance.web) > 0 ? aws_instance.web[0].public_ip : null
+  description = "Public IP of the EC2 instance (null if not created)"
 }
